59eac45ea033006422a540d5b45f1c8ab02da41b
montenegrog27 <german2717gmail.com>
2024-07-31 21:59:23 -0300

    commit 3

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import UploadButton from "./components/UploadButton";
 export default function Home() {
   return (
     <div className="flex flex-col  justify-center items-center mt-5">
-      <h1 className="text-3xl text-gray-200">File Uploader</h1>
+      <h1 className="text-3xl mb-5 text-gray-200">File Uploader</h1>
       <UploadButton />
     </div>
   );
